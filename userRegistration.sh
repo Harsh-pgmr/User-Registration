89a9027e861fda4c6be13ab43a8c21729f7a287d
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-05 11:17:33 +0530

    VAlid phone number

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,59 +1,13 @@
-#!/bin/bash/
-#FUNCTION FOR USERNAME
-function username(){
-echo "Enter User Name" 
-read word
-pat=[a-zA-z]{3,}
-#CONDITION TO CHECK VALID AND INVALID
-if [[ $word =~ $pat ]]
-then
-	echo "VALID USERNAME"
-else
-	echo "INVALID USERNAME"
-fi
-}
-
-#FUNCTION FOR LASTNAME
-function lastname(){
-echo "Enter Last Name" 
-read word
-pat=[a-zA-z]{3,}
-#CONDITION TO CHECK VALID AND INVALID
-if [[ $word =~ $pat ]]
-then
-	echo "VALID LASTNAME"
-else
-	echo "INVALID LASTNAME"
-fi
-}
-#FUNCTION FOR EMAIL
-function email(){
-	echo "Enter the email"
-	read email
-	pat="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
-
-	#CONDITION TO CHECK VALID AND INVALID
-	if [[ $email =~ $pat ]]
-	then
-		echo "VALID EMAIL"
-	else
-		echo "VALID EMAIL"
-	fi
-}
 #TO FUNCTION MOBILE
 function mobileNumber(){
 	echo "Enter the mobile number"
 	read number
-	pat="(0/91)?[7-9][0-9]{9}"
+	pattern="(0/91)?[7-9][0-9]{9}"
 	#TO CHECK THE VALID AND INVALID EMAIL
-	if [[ $number =~ $pat ]]
+	if [[ $number =~ $pattern ]]
 	then
-		echo "VALID NUMBER"
+		printf "valid number"
 	else
-		echo "VALID NUMBER"
+		printf "invalid number"
 	fi
 }
-username
-lastname
-email
-mobileNumber
