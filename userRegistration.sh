33e156783e41db9a97d9b635158da19d41880b22
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-05 12:31:30 +0530

    valid mobile number updated

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,45 @@
-#TO FUNCTION MOBILE
+#!/bin/bash/
+#FUNCTION FOR USERNAME
+function username(){
+echo "Enter User Name" 
+read word
+pat=[a-zA-z]{3,}
+#CONDITION TO CHECK VALID AND INVALID
+if [[ $word =~ $pat ]]
+then
+	echo "VALID USERNAME"
+else
+	echo "INVALID USERNAME"
+fi
+}
+#FUNCTION FOR LASTNAME
+function lastname(){
+echo "Enter Last Name" 
+read word
+pat=[a-zA-z]{3,}
+#CONDITION TO CHECK VALID AND INVALID
+if [[ $word =~ $pat ]]
+then
+	echo "VALID LASTNAME"
+else
+	echo "INVALID LASTNAME"
+fi
+}
+
+#FUNCTION FOR EMAIL
+function email(){
+	echo "Enter the email"
+	read email
+	pat="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+
+	#CONDITION TO CHECK VALID AND INVALID
+	if [[ $email =~ $pat ]]
+	then
+		echo "valid email"
+	else
+		echo "invalid email"
+	fi
+}
 function mobileNumber(){
 	echo "Enter the mobile number"
 	read number
@@ -11,3 +52,7 @@ function mobileNumber(){
 		printf "invalid number"
 	fi
 }
+username
+lastname
+email
+mobileNumber
