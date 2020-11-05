8f75296164a36941ab9e131ea31871b1565fe9d7
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-05 10:53:31 +0530

    USER REGISTRATION

--- userRegistration.sh
+++ userRegistration.sh
@@ -26,5 +26,20 @@ else
 	echo "INVALID LASTNAME"
 fi
 }
+#FUNCTION FOR EMAIL
+function email(){
+	echo "Enter the email"
+	read email
+	pat="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+
+	#CONDITION TO CHECK VALID AND INVALID
+	if [[ $email =~ $pat ]]
+	then
+		echo "valid email"
+	else
+		echo "invalid email"
+	fi
+}
 username
 lastname
+email
