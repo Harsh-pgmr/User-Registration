a47e3c9c46c94d26b29b134d6cb195cb657f3f34
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-05 12:28:54 +0530

    Valid email Updated

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,30 @@
 #!/bin/bash/
+#FUNCTION FOR USERNAME
+function username(){
+echo "Enter User Name" 
+read word
+pat=[a-zA-z]{3,}
+#CONDITION TO CHECK VALID AND INVALID
+if [[ $word =~ $pat ]]
+then
+	echo "VALID USERNAME"
+else
+	echo "INVALID USERNAME"
+fi
+}
+#FUNCTION FOR LASTNAME
+function lastname(){
+echo "Enter Last Name" 
+read word
+pat=[a-zA-z]{3,}
+#CONDITION TO CHECK VALID AND INVALID
+if [[ $word =~ $pat ]]
+then
+	echo "VALID LASTNAME"
+else
+	echo "INVALID LASTNAME"
+fi
+}
 
 #FUNCTION FOR EMAIL
 function email(){
@@ -14,4 +40,6 @@ function email(){
 		echo "invalid email"
 	fi
 }
+username
+lastname
 email
