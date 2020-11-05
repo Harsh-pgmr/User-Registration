e4783a9ea561f2f3962abaf014e8ddb38ad29f83
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-05 12:27:22 +0530

    Valid last name updated

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,17 @@
 #!/bin/bash/
+#FUNCTION FOR USERNAME
+function username(){
+echo "Enter User Name" 
+read word
+pat=[a-zA-z]{3,}
+#CONDITION TO CHECK VALID AND INVALID
+if [[ $word =~ $pat ]]
+then
+	echo "VALID USERNAME"
+else
+	echo "INVALID USERNAME"
+fi
+}
 #FUNCTION FOR LASTNAME
 function lastname(){
 echo "Enter Last Name" 
@@ -12,4 +25,5 @@ else
 	echo "INVALID LASTNAME"
 fi
 }
+username
 lastname
